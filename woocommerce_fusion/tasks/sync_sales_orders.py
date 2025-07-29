a0b5d5d5816aa6b6d2524c33663d89a0b0799893
Sukhman <sukhman@onehash.ai>
2025-07-29 11:27:18 +0530

    fix: company tax template

--- woocommerce_fusion/tasks/sync_sales_orders.py
+++ woocommerce_fusion/tasks/sync_sales_orders.py
@@ -694,32 +694,34 @@ class SynchroniseSalesOrder(SynchroniseWooCommerce):
 				try:
 					address = frappe.get_doc("Address", addresses[0].parent)
 					company_name = new_sales_order.company
+					frappe.log_error("Company Name: ", company_name) 
 
+					
+					if company_name == "WOL3D India Limited":
+						if hasattr(address, 'state') and address.state:
+							if address.state.strip().lower() == "maharashtra":
+								tax_template_name = "Output GST In-state - WIL"
+							else:
+								tax_template_name = "Output GST Out-state - WIL"
 
-					if hasattr(address, 'state') and address.state:
-						if address.state.strip().lower() == "maharashtra":
-							tax_template_name = "Output GST In-state - WIL"
-						else:
-							tax_template_name = "Output GST Out-state - WIL"
-
-						new_sales_order.taxes_and_charges = tax_template_name
-						
-						if tax_template_name:
-							tax_template = frappe.get_doc("Sales Taxes and Charges Template", tax_template_name)
-							for tax in tax_template.taxes:
-								new_sales_order.append("taxes", {
-									"charge_type": tax.charge_type,
-									"account_head": tax.account_head,
-									"description": tax.description,
-									"rate": tax.rate,
-									# "included_in_print_rate": tax.included_in_print_rate if hasattr(tax, 'included_in_print_rate') else 0,
-									"included_in_print_rate": 1,
-									"tax_amount": 0, 
-									"tax_amount_after_discount_amount": 0, 
-									"total": 0 
-								})
-						else:
-							pass
+							new_sales_order.taxes_and_charges = tax_template_name
+							
+							if tax_template_name:
+								tax_template = frappe.get_doc("Sales Taxes and Charges Template", tax_template_name)
+								for tax in tax_template.taxes:
+									new_sales_order.append("taxes", {
+										"charge_type": tax.charge_type,
+										"account_head": tax.account_head,
+										"description": tax.description,
+										"rate": tax.rate,
+										# "included_in_print_rate": tax.included_in_print_rate if hasattr(tax, 'included_in_print_rate') else 0,
+										"included_in_print_rate": 1,
+										"tax_amount": 0, 
+										"tax_amount_after_discount_amount": 0, 
+										"total": 0 
+									})
+							else:
+								pass
 				
 				except Exception as addr_error:
 					frappe.log_error(f"Error accessing address {addresses[0].parent}: {str(addr_error)}", 
